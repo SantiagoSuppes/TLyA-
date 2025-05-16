a44ef7e006e4039768dd7cadfe1e4e0461f19f74
SantiagoSuppes <slsuppes@gmail.com>
2025-05-16 13:09:15 -0300

    Guia 1

---
+++ Guia1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Guia1.Ejercicios ej = new Guia1.Ejercicios();
+
+//ej.Ej2();
+//ej.Ej3();
+//ej.Ej4a();
+//ej.Ej4b();
+//ej.Ej5();
+//ej.Ej6();
+//ej.Ej7();
+//ej.Ej8();
+//ej.Ej9();
+//ej.Ej10();
+//ej.Ej11();
