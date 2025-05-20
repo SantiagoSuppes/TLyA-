9cadf22e214207050cf2f0932560b6a7d230fac3
SantiagoSuppes <slsuppes@gmail.com>
2025-05-20 15:08:21 -0300

    Guia 3

---
+++ Guia_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Guia_3;
+
+Ejercicios ej = new Ejercicios();
+
+//ej.Ingreso30();
+//ej.Varnum();
+//ej.Principal(40, 50);
+//ej.Principal4();
+//ej.Principal5();
+//ej.Principal6();
+//ej.Principal7();
+//ej.Principal8();
+//ej.Principal9();
