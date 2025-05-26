4f4849a1f4b38998956e5e1d90da3c838c947a7d
SantiagoSuppes <slsuppes@gmail.com>
2025-05-26 15:08:29 -0300

    Guia 5

---
+++ Guia_5/Ejercicios.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia_5
+{
+    internal class Ejercicios
+    {
+        public class Persona
+        {
+            public int DNI { get; set; }
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public DateTime FechaNacimiento { get; set; }
+            public byte Edad {  get; set; }
+            public string Genero { get; set; }
+
+            public Persona()
+            {
+                Console.WriteLine("Estas utilizando el constructor sin parametros.");
+            }
+            public Persona(int dNI, string nombre, string apellido, string fechaNacimiento, byte edad, string genero)
+            {
+                this.DNI = dNI;
+                this.Nombre = nombre;
+                this.Apellido = apellido;
+                this.FechaNacimiento = Convert.ToDateTime(fechaNacimiento);
+                this.Edad = edad;
+                this.Genero = genero;
+                
+            }
+            public override string ToString()
+            {
+                return DNI + "-" + Nombre + '-' + Apellido + '-' + FechaNacimiento.ToString("dd/MM/yyyy") + '-' + Edad + '-' + Genero;
+            }
+
+            public void Saludar()
+            {
+                Console.WriteLine($"Hola Mundo! Mi nombre completo es {Nombre} {Apellido}, y tengo {Edad} anios.");
+            }
+
+            public void CumplirAnios()
+            {
+                Edad++;
+            }
+
+            public void MostrarDatos()
+            {
+                Console.WriteLine(ToString());
+            }
+
+            public bool EsMayorDeEdad()
+            {
+                bool flag = true;
+
+                if (Edad < 18)
+                {
+                    flag = false;
+                    Console.WriteLine("Ingreso denegado. Es menor de edad.");
+                }
+                else { Console.WriteLine("Ingreso admitido. Es mayor de edad."); }
+                return flag;
+            }
+        }
+    }
+}
---
+++ Guia_5/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Guia_5;using System.Globalization;
+
+//Ejercicios.Persona persona1 = new Ejercicios.Persona();
+//Console.WriteLine(persona1.ToString());
+
+Ejercicios.Persona persona2 = new Ejercicios.Persona(44966426, "Santiago", "Suppes","08/07/2003", 15, "Masculino");
+Console.WriteLine(persona2.ToString());
+
+persona2.Saludar();
+persona2.CumplirAnios();
+persona2.Saludar();
+
+persona2.MostrarDatos();
+persona2.EsMayorDeEdad();
